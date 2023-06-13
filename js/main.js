3c2fbddce83002c9f1fe65fe1b9ea8aeb566eee6
MarioSantoro <mariosantoro124@gmail.com>
2023-06-13 17:55:16 +0200

    fix the exercise

--- js/main.js
+++ js/main.js
@@ -8,12 +8,12 @@ createApp({
 
     methods:{
         generateEmail(){
-            axios.get(" https://flynn.boolean.careers/exercises/api/random/mail")
-            .then( (email) => {
-               for(let i= 0 ; i < 10 ; i++){
+            for(let i= 0 ; i < 10 ; i++){
+                axios.get(" https://flynn.boolean.careers/exercises/api/random/mail")
+                .then( (email) => {
                     this.emailList.push(email.data.response)
-               }
-        });
+            });
+            }
         }
     },
 
